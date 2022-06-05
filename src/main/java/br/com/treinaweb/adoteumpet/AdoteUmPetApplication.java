978fdad2d1d16ca8c0bfa103b856b2c7b8375e4b
Gilbertoaleite <gilbertoaleite@hotmail.com>
2022-06-05 20:25:01 -0300

    finalizado a api

--- src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
+++ src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
@@ -1,13 +1,29 @@
 package br.com.treinaweb.adoteumpet;
 
+import br.com.treinaweb.adoteumpet.core.models.Pet;
+import br.com.treinaweb.adoteumpet.core.repositories.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdoteUmPetApplication {
+public class AdoteUmPetApplication implements CommandLineRunner {
+
+	@Autowired
+	private PetRepository petRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdoteUmPetApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var pet = new Pet();
+		pet.setNome("Joãzinho");
+		pet.setHistoria("Historia emocionante");
+		pet.setFoto("http://www.exemplo.com.br/foto.png");
+		petRepository.save(pet);
 	}
 
 }

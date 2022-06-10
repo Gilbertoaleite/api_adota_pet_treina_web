84c10f657465d1fe38d37c934a76c29672b63cb6
Gilbertoaleite <gilbertoaleite@hotmail.com>
2022-06-10 19:04:10 -0300

    removendo do petaplication o injection de cadastro inicial

--- src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
+++ src/main/java/br/com/treinaweb/adoteumpet/AdoteUmPetApplication.java
@@ -1,29 +1,18 @@
 package br.com.treinaweb.adoteumpet;
 
-import br.com.treinaweb.adoteumpet.core.models.Pet;
 import br.com.treinaweb.adoteumpet.core.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdoteUmPetApplication implements CommandLineRunner {
+public class AdoteUmPetApplication {
 
 	@Autowired
 	private PetRepository petRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdoteUmPetApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		var pet = new Pet();
-		pet.setNome("Leka");
-		pet.setHistoria("Muito animada, adora brincar de bolinha");
-		pet.setFoto("https://imgur.com/vDYz7qp.jpeg");
-		petRepository.save(pet);
 	}
 
 }
